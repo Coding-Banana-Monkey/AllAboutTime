f224d4e001e58a51d12a71fc4ccf22b98674e58e
Coding-Coco-Monkey <coding.c.monkey@gmail.com>
2018-03-14 21:36:00 -0700

    served html

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
 var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var filePath = path.join(__dirname, 'html/clock.html');
 
 http.createServer(function(req, res) {
-  res.write('Fuck U');
-  res.end('\nDone');
+  res.writeHead(200, {
+          "Content-Type": "text/html"
+      });
+
+  fs.createReadStream(filePath).pipe(res);
+  //res.write('Fuck U');
+  // res.end('\nConnected...');
 }).listen(8080);
